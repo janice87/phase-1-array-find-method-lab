0b6a8e76fc5cf4a779ddcaf9f043cba97561d763
Janice Chan <janice.chan1288@gmail.com>
2022-03-20 13:31:45 -0400

    commit tree a52ed6a1f031192b0ce9921d1ee1ba44f735c52c

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+const superbowlWin = function(array) {
+    const found = array.find(obj => obj.result === "W") 
+    if(found) {
+        return found.year
+    }  
+}
+console.log(found)   
